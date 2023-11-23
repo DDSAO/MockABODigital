872f019666b4f87b9bb7d8f332d8de9225b3a56d
Harry@mac <Harry@mac>
2023-11-24 10:33:15 +1100

    u

---
+++ app/_components/SmoothWrapper/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollTrigger, gsap } from "gsap/all";
+import { useEffect } from "react";
+
+export const SmoothWrapper = ({
+  children,
+}: {
+  children: React.ReactNode | React.ReactNode[];
+}) => {
+  useEffect(() => {
+    gsap.config({ trialWarn: false } as any);
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+    });
+  }, []);
+
+  return (
+    <div id="smooth-wrapper">
+      <div id="smooth-content">{children}</div>
+    </div>
+  );
+};
